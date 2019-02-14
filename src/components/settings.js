da5f3a8b57b54f17861731e9c0936a3a7e927c42
Michael Rodgers <mail@mjrodge.me>
2019-02-14 12:33:51 -0500

    Setup Input Components

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import { Button, View, Text } from 'react-native';
+import { Card, CheckBox, Slider } from 'react-native-elements';
 
 class Settings extends React.Component {
   static navigationOptions = {
     title: 'Settings',
   };
+
+  state = {
+    lbChecked: true,
+    kgChecked: false,
+    ftChecked: true,
+    cmChecked: false,
+    value: 80,
+  };
   render() {
+    const { cardStyle } = styles;
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Settings Screen</Text>
+      <View style={{ flex: 1 }}>
+        <Card title="Weight Unit" style={cardStyle}>
+          <CheckBox
+            center
+            title="Pounds (lb)"
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            checked={this.state.lbChecked}
+          />
+          <CheckBox
+            center
+            title="Kilograms (kg)"
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            checked={this.state.kgChecked}
+          />
+        </Card>
+        <Card title="Height Unit" style={cardStyle}>
+          <CheckBox
+            center
+            title="Feet/Inches (ft/in)"
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            checked={this.state.ftChecked}
+          />
+          <CheckBox
+            center
+            title="Centimetres (cm)"
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            checked={this.state.cmChecked}
+          />
+        </Card>
+        <Card title="Fat/Carb Split">
+          <Slider
+            value={this.state.value}
+            onValueChange={value => this.setState({ value })}
+            maximumValue={100}
+            step={1}
+            onSlidingComplete={() => {}}
+          />
+          <Text>Carbohydrate percentage: {this.state.value}%</Text>
+        </Card>
       </View>
     );
   }
 }
+const styles = {
+  cardStyle: {
+    flex: 1,
+  },
+};
 
 export default Settings;
