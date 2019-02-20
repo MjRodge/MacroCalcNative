bd1d490093e8f6a205a698a2e54f2adaed480248
Michael Rodgers <mail@mjrodge.me>
2019-02-20 12:31:26 -0500

    Removed Button Import

--- src/components/results.js
+++ src/components/results.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 class Results extends React.Component {
   static navigationOptions = {
