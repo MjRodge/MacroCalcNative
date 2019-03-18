23889a4252c7c0324b9d35ad360b3006f41cbb58
Michael Rodgers <mail@mjrodge.me>
2019-03-18 17:28:56 -0400

    ESLint Rules Applied

--- src/components/common/chartLegend.js
+++ src/components/common/chartLegend.js
@@ -2,12 +2,29 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const ChartLegend = ({ color, text, data }) => {
-    return (
-        <View style={{ flexDirection: 'row', margin: 4, alignItems: 'center', justifyContent: 'center' }}>
-            <View style={{ height: 10, width: 10, backgroundColor: color, padding: 4, margin: 5 }} />
-            <Text>{data} {text}</Text>
-        </View>
-    );
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        margin: 4,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <View
+        style={{
+          height: 10,
+          width: 10,
+          backgroundColor: color,
+          padding: 4,
+          margin: 5
+        }}
+      />
+      <Text>
+        {data} {text}
+      </Text>
+    </View>
+  );
 };
 
 export { ChartLegend };
