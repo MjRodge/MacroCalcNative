51ad98f3d709716b31aabc7c0d69f82af84c17cd
Michael Rodgers <mail@mjrodge.me>
2019-03-22 11:38:03 -0400

    Added ThemeProvider to Style react-native-elements

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
+import { ThemeProvider } from 'react-native-elements';
 import { persistor, reduxStore } from './src/store';
+import { theme } from './src/theme';
 
 import MainStackNavigator from './src/navigation/AppNavigator';
-
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={reduxStore}>
         <PersistGate loading={null} persistor={persistor}>
-          <MainStackNavigator />
+          <ThemeProvider theme={theme}>
+            <MainStackNavigator />
+          </ThemeProvider>
         </PersistGate>
       </Provider>
     );
---
+++ src/theme/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    primary: '#ff9800'
+  },
+  Button: {
+    raised: true
+  },
+  Slider: {
+    minimumTrackTintColor: '#00BCD4',
+    thumbTintColor: '#ff9800'
+  }
+};
