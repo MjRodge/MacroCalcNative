5e22be3fe6cca0403e848f4d239545733788c33c
Michael Rodgers <mail@mjrodge.me>
2019-04-09 12:39:02 -0400

    Added SearchBar and Redux

--- src/screens/foodSearchScreen.js
+++ src/screens/foodSearchScreen.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { SearchBar } from 'react-native-elements';
 
-export default class FoodSearchScreen extends React.Component {
+import { foodSearchAutoComplete } from '../actions';
+
+class FoodSearchScreen extends React.Component {
   static navigationOptions = {
     title: 'Food Search'
   };
+  state = {
+    search: ''
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+    this.props.foodSearchAutoComplete(search);
+  };
 
   render() {
+    const { search } = this.state;
+
     return (
-      <ScrollView>
-        <Text>Ello World</Text>
-      </ScrollView>
+      <View style={{ flex: 1 }}>
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={this.updateSearch}
+          value={search}
+          lightTheme
+          round
+        />
+        <Text>{this.props.searchQuery}</Text>
+      </View>
     );
   }
 }
+
+const mapStateToProps = state => {
+  const { searchQuery } = state.food;
+  return {
+    searchQuery
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { foodSearchAutoComplete }
+)(FoodSearchScreen);
