290a364f29e2affc4eb535c7595eabc46ee73bb3
Michael Rodgers <mail@mjrodge.me>
2019-04-09 16:26:46 -0400

    Applied Redux-Thunk as Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
@@ -6,12 +7,12 @@ import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import reducers from '../reducers';
 
 const persistConfig = {
- key: 'root',
- storage,
- stateReconciler: autoMergeLevel2
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2
 };
 
 const pReducer = persistReducer(persistConfig, reducers);
 
-export const reduxStore = createStore(pReducer);
+export const reduxStore = createStore(pReducer, {}, applyMiddleware(ReduxThunk));
 export const persistor = persistStore(reduxStore);
